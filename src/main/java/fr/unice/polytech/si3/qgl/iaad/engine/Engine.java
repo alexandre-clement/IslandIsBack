a5e21c64fa3aa602d2fef5dc2639cc3cf64eaaa9
alexandre-clement <alexandre.didier.clement@gmail.com>
2017-02-09 16:38:48 +0100

    Engine now stop when the contract is completed or if the contract is empty.

--- src/main/java/fr/unice/polytech/si3/qgl/iaad/engine/Engine.java
+++ src/main/java/fr/unice/polytech/si3/qgl/iaad/engine/Engine.java
@@ -4,6 +4,7 @@ import fr.unice.polytech.si3.qgl.iaad.common.StopGame;
 import fr.unice.polytech.si3.qgl.iaad.decisions.Decision;
 import fr.unice.polytech.si3.qgl.iaad.format.Context;
 import fr.unice.polytech.si3.qgl.iaad.format.Result;
+import fr.unice.polytech.si3.qgl.iaad.resource.Contract;
 import fr.unice.polytech.si3.qgl.iaad.strategy1.Strategy1;
 
 /**
@@ -16,16 +17,25 @@ public class Engine
     private static final String REPORT = "We're gonna be rich !";
     private Protocol protocol;
     private int budget;
+    private final Context context;
 
     public Engine(Context context)
     {
         this.protocol = new Strategy1(context);
         this.budget = context.getBudget();
+        this.context = context;
     }
 
     public Decision takeDecision()
     {
+        checkIfContractIsCompleted();
         return protocol.takeDecision();
+    }
+
+    private void checkIfContractIsCompleted()
+    {
+        if (context.getContracts().stream().allMatch(Contract::complete))
+            protocol = new StopGame();
     }
 
     public void acknowledgeResults(Result result)
